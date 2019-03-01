dfa8ca4674daa22d82aafd21100d0dc4a978ba71
Christian Pauly <christian-pauly@posteo.de>
2019-03-01 10:50:36 +0100

    Fixes

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -9,21 +9,49 @@ window.onload = function(e){
 		$("#header_main_nav").slideUp();
 	});
 
-	$(document).ready(function() {
-		$(".fancybox").fancybox();
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 900, 'swing', function () {
+			window.location.hash = target;
+		});
 	});
 
-	$('a[href^="#"]').on('click',function (e) {
-    e.preventDefault();
+}
 
-    var target = this.hash;
-    var $target = $(target);
+function switchToCorrectLanguage () {
 
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 900, 'swing', function () {
-        window.location.hash = target;
-    });
-});
+	/* Switch to correct language */
+	var splittedUrl = document.documentURI.split("/");
+
+	// Detect the current language
+	var currentLanguage = splittedUrl[splittedUrl.length-1];
+	if ( window.location.href.endsWith (".html") ) {
+		currentLanguage = splittedUrl[splittedUrl.length-2];
+	}
+
+	// If there is already a language selected then stop here
+	if ( languages.indexOf( currentLanguage ) !== -1 ) return;
+
+	// Get the preferred language
+	var preferredLanguageIndex = languages.indexOf(navigator.language.substr(0,2))
+
+	if ( preferredLanguageIndex !== -1 ) {
+		var preferredLanguage = languages [ preferredLanguageIndex ]
+		if ( window.location.href.endsWith (".html") ) {
+			splittedUrl[splittedUrl.length] = splittedUrl[splittedUrl.length-1];
+			splittedUrl[splittedUrl.length-2] = preferredLanguage;
+			window.location.href = splittedUrl.join("/")
+		}
+		else window.location.href += preferredLanguage;
+	}
 
 }
+
+
+switchToCorrectLanguage ()
